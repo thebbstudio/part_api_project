ab25d547279b30b4a35903973d1d68b27ee46ac0
Aleksandr <mamamacriminal@yandex.ru>
2022-04-16 21:02:29 +0500

    asd

--- patr_api/client/src/pages/ErrorPageNotFound.jsx
+++ patr_api/client/src/pages/ErrorPageNotFound.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import Header from '../components/Header';
+import Navbar from '../components/Navbar';
+
+const style = {
+  margin: '50px 0 0',
+  // display: 'flex',
+  // textAlign: 'center',
+  // justifyContent: 'center',
+};
 
 function ErrorPageNotFound() {
   return (
-    <div>ErrorPageNotFound</div>
+    <>
+      <Header />
+      <Navbar />
+      <div className="container">
+        <h1 style={{ ...style }}>Ошибка - Страница не найдена</h1>
+      </div>
+    </>
   );
 }
 
