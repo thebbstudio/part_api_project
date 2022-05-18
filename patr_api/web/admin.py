1d6b4e8b0b08aa173598084d9e4e237b0390274e
Sergey <45151835+thebbstudio@users.noreply.github.com>
2022-05-19 01:30:14 +0500

    "Готова" админ панель для загрузки нескольких фото
    Нужно продублировать для мероприятий, а так же отправку нескольких фото по API

--- patr_api/web/admin.py
+++ patr_api/web/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class NewsImageAdmin(admin.StackedInline):
+    model = NewsImage
+    extra = 1
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    inlines = [NewsImageAdmin]
+    list_display = ('id', 'title', 'date_publication', 'isActive')
+    list_display_links = ('title',)
+
+
+@admin.register(NewsImage)
+class NewsImageAdmin(admin.ModelAdmin):
+    pass
+
+
+
+@admin.register(Park)
+class ParkAdmin(admin.ModelAdmin):
+    list_display = ('title', 'park_info')
+ 
+ 
+
+admin.site.register(Events)
+admin.site.register(Staff)
+admin.site.register(Partners)
--- patr_api/web/models.py
+++ patr_api/web/models.py
@@ -1,6 +1,7 @@
 from distutils.command.upload import upload
 from django.db import models
 from datetime import date
+from patr_api.settings import MEDIA_ROOT
 # news
 # events
 # documents
@@ -14,12 +15,33 @@ from datetime import date
 class News(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    img_path = models.ImageField(upload_to=('img/news/' + str(date.today())))
+    img_path = models.ImageField(upload_to=('img/news/' + str(date.today())), blank=True)
     date_publication = models.DateTimeField()
     education_departament = models.BooleanField(default=False)
-    is_slider = models.BooleanField(default=False)
-    imgs_path = models.CharField(max_length=255)
     video = models.TextField(default='')
+    isActive = models.BooleanField(default=True)
+    @property
+    def get_absolute_image_url(self):
+        return "{0}{1}".format(MEDIA_ROOT, self.img_path.url)
+    class Meta:
+        ordering = ['-date_publication', 'title']
+
+
+class NewsImage(models.Model):
+    news = models.ForeignKey(News, on_delete=models.CASCADE, default=None)
+    path = models.FileField(upload_to='img/news/heap/', default=None, blank=True)
+
+
+class Events(models.Model):
+    title = models.CharField(max_length=100)
+    img_path = models.ImageField(upload_to=('img/event/' + str(date.today())))
+    description = models.TextField()
+    date_publication = models.DateTimeField()
+    short_text = models.CharField(max_length=500)
+    is_slider = models.BooleanField(default=False)
+    imgs_path = models.CharField(max_length=255, default=None)
+    video = models.TextField(default='')
+    isActive = models.BooleanField(default=True)
 
     class Meta:
         ordering = ['-date_publication', 'title']
@@ -30,22 +52,13 @@ class PaidServise(models.Model):
     cost = models.IntegerField()
     imgPath = models.ImageField(upload_to='img/paidservise/')
     dimensionMeasurement = models.CharField(max_length=50)
-    isActive =models.BooleanField(default=True)
-
-class Events(models.Model):
-    title = models.CharField(max_length=100)
-    img_path = models.ImageField(upload_to=('img/event/' + str(date.today())))
-    description = models.TextField()
-    date_publication = models.DateTimeField()
-    short_text = models.CharField(max_length=500)
-    is_slider = models.BooleanField(default=False)
-    imgs_path = models.CharField(max_length=255)
-    video = models.TextField(default='')
+    isActive = models.BooleanField(default=True)
 
 class Documents(models.Model):
     title = models.CharField(max_length=150)
     href_string = models.FileField(upload_to='docs/')
     category = models.IntegerField()
+    isActive = models.BooleanField(default=True)
 
 
 class DocType(models.Model):
@@ -53,18 +66,22 @@ class DocType(models.Model):
     importance = models.IntegerField(default=None)
 
 
-class Staff (models.Model):
+class Staff(models.Model):
     full_name = models.CharField(max_length=150)
     img_path = models.ImageField(upload_to='img/staff/')
     href_string = models.CharField(max_length=255)
     position = models.CharField(max_length=150)
     importance = models.IntegerField()
+    isActive = models.BooleanField(default=True)
 
+    class Meta:
+        ordering = ['importance', 'full_name']
 
-class Partners (models.Model):
+class Partners(models.Model):
     img_path = models.ImageField()
     title = models.CharField(max_length=100)
     href_string = models.ImageField(upload_to='img/partners/')
+    isActive = models.BooleanField(default=True)
 
 
 class Videos(models.Model):
@@ -81,12 +98,14 @@ class Park(models.Model):
     category_id = models.IntegerField()
     is_slider = models.BooleanField(default=False)
     img_paths = models.ImageField(upload_to='img/park/', default='default.jpg')
+    isActive = models.BooleanField(default=True)
 
 
 class ParkCategories(models.Model):
     title = models.CharField(max_length=100)
     url = models.CharField(max_length=250, default='')
     img_path = models.ImageField('img/park/categories/')
+    isActive = models.BooleanField(default=True)
 
 
 
