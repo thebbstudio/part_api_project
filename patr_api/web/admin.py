c227e5f8b0b140c8bc11e292351c4fe7a969c4fe
Sergey <45151835+thebbstudio@users.noreply.github.com>
2022-05-21 21:54:15 +0500

    Изменил admin панель
    Добавил возможность изменить список платных услуг

--- patr_api/web/admin.py
+++ patr_api/web/admin.py
@@ -18,13 +18,40 @@ class NewsImageAdmin(admin.ModelAdmin):
     pass
 
 
+class EventsImageAdmin(admin.StackedInline):
+    model = EventsImage
+    extra = 1
+
+@admin.register(Events)
+class EventsAdmin(admin.ModelAdmin):
+    inlines = [EventsImageAdmin]
+    list_display = ('id', 'title', 'date_publication', 'isActive')
+    list_display_links = ('title',)
+
+
+@admin.register(EventsImage)
+class EventsImageAdmin(admin.ModelAdmin):
+    pass
+
 
 @admin.register(Park)
 class ParkAdmin(admin.ModelAdmin):
     list_display = ('title', 'park_info')
  
- 
 
-admin.site.register(Events)
-admin.site.register(Staff)
-admin.site.register(Partners)
+@admin.register(Staff)
+class StaffAdmin(admin.ModelAdmin):
+    list_display = ('full_name','importance','isActive')
+    list_display_links = ('full_name',)
+
+
+@admin.register(Partners)
+class PartnersAdmin(admin.ModelAdmin):
+    list_display = ('title', 'isActive')
+    list_display_links = ('title',)
+
+
+@admin.register(PaidServise)
+class PaidServiseAdmin(admin.ModelAdmin):
+    list_display = ('title','cost','isActive')
+    list_display_links = ('title',)
