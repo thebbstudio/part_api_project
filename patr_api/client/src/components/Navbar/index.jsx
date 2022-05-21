a6d091d3531a85460ab2a3d6d64695d41f23ab56
Aleksandr <mamamacriminal@yandex.ru>
2022-05-21 22:07:34 +0500

    navbar http

--- patr_api/client/src/components/Navbar/index.jsx
+++ patr_api/client/src/components/Navbar/index.jsx
@@ -1,27 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import HttpService from '../../http/HttpService';
 import cl from './Navbar.module.css';
 
 function Navbar() {
-  const [nav, setNav] = useState([
-    { title: 'Главная', path: '/' },
-    { title: 'Новости', path: '/news' },
-    { title: 'Мероприятия', path: '/events' },
-    { title: 'Парк', path: '/park' },
-    { title: 'Сотрудники', path: '/staff' },
-    { title: 'Услуги', path: '/paidservices' },
-    { title: 'Документация', path: '/documentation' },
-  ]);
+  const [nav, setNav] = useState([]);
   const [burgerChecked, setBurgerCheched] = useState(false);
   const handleInput = (e) => {
     setBurgerCheched(e.target.checked);
   };
 
-  useEffect(() => {
+  async function getNavbar() {
+    const http = await HttpService.getNavbar({ format: 'json' });
+    setNav(http.data);
+  }
+
+  function addEventCloseBurger() {
     const list = document.querySelector(`.${cl.list}`);
     list.addEventListener('click', () => {
       setBurgerCheched(false);
     });
+  }
+
+  useEffect(() => {
+    addEventCloseBurger();
+    getNavbar();
   }, []);
 
   return (
